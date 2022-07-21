2fe1c196f1a1648744c8590d83780b99a0622dab
amitgawande369 <109359073+amitgawande369@users.noreply.github.com>
2022-07-21 13:04:46 +0530

    Create solution.java

---
+++ Data Structures/Java Priority Queue/solution.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.*;
+/*
+ * Create the Student and Priorities classes here.
+ */
+class Student
+{
+    int id; 
+    String name; 
+    double cgpa;
+    Student(int id, String name, double cgpa)
+    {
+        this.id = id;
+        this.name = name;
+        this.cgpa = cgpa;       
+    }
+    int getID()
+    {
+        return id;
+    }
+    String getName()
+    {
+        return name;
+    }
+    double getCGPA()
+    {
+        return cgpa;
+    }
+}
+class Priorities
+{
+    private final PriorityQueue<Student> queue = new PriorityQueue<>(
+            Comparator.comparing(Student::getCGPA).reversed()
+                    .thenComparing(Student::getName)
+                    .thenComparing(Student::getID));
+
+    public List<Student> getStudents(List<String> events) 
+    {
+        events.forEach((event) -> {
+            if (event.equals("SERVED")) {
+                queue.poll();
+            } else {
+                String[] details = event.split(" ");
+
+                queue.add(new Student(Integer.parseInt(details[3]), details[1], Double.parseDouble(details[2])));
+            }
+        });
+
+        List<Student> students = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            students.add(queue.poll());
+        }
+
+        return students;
+    }    
+}
+
