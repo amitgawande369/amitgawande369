fb9ada46c4a467f1d79dcf9db07aff9c6c46f635
amitgawande369 <109359073+amitgawande369@users.noreply.github.com>
2022-07-21 16:26:05 +0530

    Create solution.java

---
+++ Data Structures/Java Stdin and Stdout II/solution.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try
+        {
+        int a = Integer.parseInt(br.readLine());
+        //sc.nextLine();
+        double d = Double.parseDouble(br.readLine());
+        //sc.nextLine();
+        String s = br.readLine();
+        //br.readLine();
+        //String s = br.readLine();
+        System.out.println("String: " +s);
+        System.out.println("Double: " +d);
+        System.out.println("Int: " +a);
+        // }*/
+        /*int a = sc.nextInt();
+        sc.nextLine();
+        double d = sc.nextDouble();
+        sc.nextLine();
+        String s = sc.nextLine();
+        //br.readLine();
+        //String s = br.readLine();
+        System.out.println("String: " +s);
+        System.out.println("Double: " +d);
+        System.out.println("Int: " +a);
+        // }*/
+        }
+        catch(IOException io)
+        {
+            
+        }
+    }
+}
